9da074808a73733dcd1e4f725b2de74f000bbefa
hailMeh <sultanhabibi@mail.ru>
2021-03-01 09:57:51 +0300

    test from other system

--- src/Components/profile/post/post.jsx
+++ src/Components/profile/post/post.jsx
@@ -2,7 +2,7 @@ import post from './post.module.css'
 
 const NewPost = (props) => {
     return <div>
-        <div className={post.profile__Circle}>
+        <div className={post.profile__img}>
             <p className={post.profile__MyNews}>{props.message} {props.messagetry}
             </p>
         </div>
