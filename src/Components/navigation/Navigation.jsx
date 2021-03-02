a29e79af77bb5bdfac881d48494e078ae689f2cb
hailMeh <Sultanhabibi@mail.ru>
2021-03-02 16:49:11 +0300

    added NavLink and active class for buttons

--- src/Components/navigation/Navigation.jsx
+++ src/Components/navigation/Navigation.jsx
@@ -1,10 +1,12 @@
 import navi from './navigation.module.css';
+import {NavLink} from "react-router-dom";
+
 const Navigation = () => {
-    return(
+    return (
         <div className={navi.navigation}>
             <ul className={navi.list}>
-                <li><a href={"/profile/profile.jsx"}>Profile </a></li>
-                <li><a href={"/dialogs/dialogs.jsx"}>Messages</a></li>
+                <li><NavLink to='/profile/profile.jsx' activeClassName={navi.activeLink}>Profile </NavLink></li>
+                <li><NavLink to='/dialogs/dialogs.jsx' activeClassName={navi.activeLink}>Messages</NavLink></li>
                 <li>News</li>
                 <li>Music</li>
                 <li className={navi.settings}>Settings</li>
